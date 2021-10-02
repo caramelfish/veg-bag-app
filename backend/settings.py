e9a44267ece6070ecdfff24cda1c58110b39c22d
Cara <carajayne11@gmail.com>
2021-10-03 00:13:16 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -149,6 +149,8 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "build/static")]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
